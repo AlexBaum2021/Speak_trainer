15cca25cf5922f6de2f1c9a97c3305458012fa07
Alex <alexbaum2021@gmail.com>
2024-11-11 11:26:09 +0100

    Initial Commit

---
+++ Speak trainer/Speak_trainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speak_trainerApp.swift
+//  Speak trainer
+//
+//  Created by Alexander Baum on 11.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Speak_trainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Speak trainerTests/Speak_trainerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Speak_trainerTests.swift
+//  Speak trainerTests
+//
+//  Created by Alexander Baum on 11.11.24.
+//
+
+import Testing
+@testable import Speak_trainer
+
+struct Speak_trainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
